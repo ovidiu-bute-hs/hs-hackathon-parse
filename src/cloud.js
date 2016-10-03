b9322cbb525e745b10e8148657abbafd831396a2
ovidiu-bute-hs <ovidiu.bute@hootsuite.com>
2016-10-03 21:14:54 +0300

    Fix decrement below 0.

--- src/cloud.js
+++ src/cloud.js
@@ -2,8 +2,15 @@ Parse.Cloud.afterSave("Person", function(request) {
   query = new Parse.Query("BucharestOffice")
   query.first({
     success: function(office) {
-      post.increment('online', request.object.get('person').inOffice ? 1 : -1)
-      post.save()
+      const personInOffice = request.object.get('person').inOffice
+
+      // Do not decrement past 0
+      if (!office.online && !personInOffice) {
+        return
+      }
+
+      office.increment('online', request.object.get('person').inOffice ? 1 : -1)
+      office.save()
     },
     error: function(error) {
       console.error(`Got an error ${error.code} : ${error.message}`)
