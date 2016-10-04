92f7b8dd1e74bac03e1ba5a78578a155791ee28c
ovidiu-bute-hs <ovidiu.bute@hootsuite.com>
2016-10-04 10:39:36 +0300

    Fix online counter.

--- src/cloud.js
+++ src/cloud.js
@@ -2,15 +2,15 @@ Parse.Cloud.afterSave("Person", function(request) {
   query = new Parse.Query("BucharestOffice")
   query.first({
     success: function(office) {
-      const personInOffice = request.object.get('person').inOffice
+      const personInOffice = request.object.get('isInOffice')
 
       // Do not decrement past 0
-      if (!office.online && !personInOffice) {
+      if (office.get('online') <= 0 && personInOffice === false) {
         return
       }
 
-      office.increment('online', request.object.get('person').inOffice ? 1 : -1)
-      office.save()
+      office.increment('online', personInOffice === true ? 1 : -1)
+      return office.save()
     },
     error: function(error) {
       console.error(`Got an error ${error.code} : ${error.message}`)
